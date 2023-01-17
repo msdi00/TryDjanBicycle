effe83e1cb49e46517095f14fe00d0faeeda33e2
msdi0 <msdi00@mail.ru>
2023-01-17 18:00:26 +0300

    Added includes

--- bicycle/urls.py
+++ bicycle/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('', views.get_bicycle, name='name-bike'),
     path('/<str:model>', views.get_description, name='name-model'),
